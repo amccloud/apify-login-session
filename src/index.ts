8285bad2c4e40084c11cd0ebf33b16efced0534c
Andrew McCloud <andrew@devulge.com>
2020-08-16 12:07:10 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -160,6 +160,18 @@ Apify.main(async () => {
         handlePageTimeoutSecs: 300,
         persistCookiesPerSession: false,
         handlePageFunction: async ({ page, request, response }) => {
+            // This is a Puppeteer function that takes a screenshot of the page and returns its buffer.
+            const screenshotBuffer = await page.screenshot();
+
+            // The record key may only include the following characters: a-zA-Z0-9!-_.'()
+            const key = request.url.replace(/[:/]/g, '_');
+
+            // Save the screenshot. Choosing the right content type will automatically
+            // assign the local file the right extension, in this case .png.
+            // The screenshots will be stored in ./apify_storage/key_value_stores/default/
+            await Apify.setValue(key, screenshotBuffer, { contentType: 'image/png' });
+            console.log(`Screenshot of ${request.url} saved.`);
+            
             try {
                 usedSession!.setCookiesFromResponse(response);
             } catch (e) {
